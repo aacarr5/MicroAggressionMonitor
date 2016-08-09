51d00ba035ae02df3763ce8c6103036c5057b011
Andrew Carr <aacarr5@gmail.com>
2016-08-09 16:50:47 -0500

    Add relevent user attributes

---
+++ app/controllers/concerns/demographics.rb
@@ -0,0 +1,8 @@
+module Demographics
+
+	GENDERS = ["Other","Non-gender Conforming","Gender-fluid","Agender","Trans-Femme","Trans-Masc","Female","Male","Unsure"].freeze
+	RESPONSES = %w(Yes No Maybe).freeze
+	RACES = ['Other',"Black", "Asian","Indian","Mixed-Race","Latino/Latina", "Native-American","White"].freeze
+	INDUSTRIES = %w(Real-estate Healthcare/Medicine Law Technology Marketing/Advertising Finance Non-profit Retail Food-industry Government Transportation Education Agriculture Mining Construction Arts/Entertainment Social-Services Other).freeze
+
+end
